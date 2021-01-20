d01092a0a393ad93a76874dad92033280846124c
JoniLaine <joni.tk.laine@gmail.com>
2021-01-20 13:54:51 +0200

    package.json modified for webapp deployment

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use('/name', nameRouter);
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Listening port ${PORT}`);
 });
